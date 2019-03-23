c5af00c906a2594f03bb29e3fb9a9267e2624d05
crimson666 <ankurbsws@gmail.com>
2019-03-23 17:07:05 +0530

    slider Stuffs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'slider',
     pathMatch: 'full'
   },
   {
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'slider', loadChildren: './slider/slider.module#SliderPageModule' }
 ];
 
 @NgModule({
